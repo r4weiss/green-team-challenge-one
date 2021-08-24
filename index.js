6eac2e9dd0367e6fd984615f170e4d28b898ce38
Virgil Nilson <virgilnilson@gmail.com>
2021-08-24 11:09:30 +0300

    Add Calendly integration to reservation buttons and links

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+function OpenCalendly() {
+  Calendly.initPopupWidget({
+    url: "https://calendly.com/chefcarminenolls/personal-chef?hide_event_type_details=1&hide_gdpr_banner=1&primary_color=ecdabf",
+  });
+  return false;
+}
 
+window.onload = function () {
+  var links = document.getElementsByClassName("reservations");
+  for (var i = 0; i < links.length; i++) {
+    var link = links[i];
+    link.onclick = function () {
+      OpenCalendly();
+    };
+  }
+};
