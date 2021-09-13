5de7c33e83ec973a9faba307db81616f08fa5290
Virgil Nilson <virgilnilson@gmail.com>
2021-09-13 10:38:38 +0300

    Fix multiple declarations of video in index.js, links now work, smooth scrolling link behavior, reservations button working again

--- index.js
+++ index.js
@@ -1,86 +1,96 @@
 function OpenCalendly() {
-	Calendly.initPopupWidget({
-		url: "https://calendly.com/chefcarminenolls/personal-chef?hide_event_type_details=1&hide_gdpr_banner=1&primary_color=ecdabf",
-	})
-	return false
+  Calendly.initPopupWidget({
+    url: "https://calendly.com/chefcarminenolls/personal-chef?hide_event_type_details=1&hide_gdpr_banner=1&primary_color=ecdabf",
+  });
+  return false;
 }
 
 window.onload = function () {
-	var reservationLinks = document.getElementsByClassName("reservations")
-	for (var i = 0; i < reservationLinks.length; i++) {
-		var link = reservationLinks[i]
-		link.onclick = function (e) {
-			OpenCalendly()
-			e.preventDefault()
-		}
-	}
+  var reservationLinks = document.getElementsByClassName("reservations");
+  for (var i = 0; i < reservationLinks.length; i++) {
+    var link = reservationLinks[i];
+    link.onclick = function (e) {
+      OpenCalendly();
+      e.preventDefault();
+    };
+  }
 
-	var footerReservationLinks = document.getElementsByClassName(
-		"footer-reservations"
-	)
-	for (var i = 0; i < footerReservationLinks.length; i++) {
-		var link = footerReservationLinks[i]
-		link.onclick = function (e) {
-			OpenCalendly()
-			e.preventDefault()
-		}
-	}
+  var footerReservationLinks = document.getElementsByClassName(
+    "footer-reservations"
+  );
+  for (var i = 0; i < footerReservationLinks.length; i++) {
+    var link = footerReservationLinks[i];
+    link.onclick = function (e) {
+      OpenCalendly();
+      e.preventDefault();
+    };
+  }
 
-	const hamburger = document.querySelector(".hamburger")
-	const navMenu = document.querySelector(".nav-menu")
-	const navLink = document.querySelectorAll(".nav-link")
-	hamburger.addEventListener("click", mobileMenu)
-	navLink.forEach((n) => n.addEventListener("click", closeMenu))
+  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
 
-	function mobileMenu() {
-		hamburger.classList.toggle("active")
-		navMenu.classList.toggle("active")
-	}
+      document.querySelector(this.getAttribute("href")).scrollIntoView({
+        behavior: "smooth",
+      });
+    });
+  });
 
-	function closeMenu() {
-		hamburger.classList.remove("active")
-		navMenu.classList.remove("active")
-	}
+  const hamburger = document.querySelector(".hamburger");
+  const navMenu = document.querySelector(".nav-menu");
+  const navLink = document.querySelectorAll(".nav-link");
+  hamburger.addEventListener("click", mobileMenu);
+  navLink.forEach((n) => n.addEventListener("click", closeMenu));
 
-	//hero-video
-	const video = document.querySelector(".hero-video")
-	video.addEventListener("ended", function () {
-		this.classList.remove("fade-in")
-		this.classList.add("fade-out")
-		setTimeout(() => video.play(), 1300)
-	})
+  function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+  }
 
-	video.addEventListener("playing", function () {
-		this.classList.add("fade-in")
-		this.classList.remove("fade-out")
-	})
+  function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  }
 
-	//process-video
-	const video = document.querySelector(".process-video")
-	video.addEventListener("ended", function () {
-		this.classList.remove("fade-in")
-		this.classList.add("fade-out")
-		setTimeout(() => video.play(), 1300)
-	})
+  //hero-video
+  const heroVideo = document.querySelector(".hero-video");
+  heroVideo.addEventListener("ended", function () {
+    this.classList.remove("fade-in");
+    this.classList.add("fade-out");
+    setTimeout(() => heroVideo.play(), 1300);
+  });
 
-	video.addEventListener("playing", function () {
-		this.classList.add("fade-in")
-		this.classList.remove("fade-out")
-	})
+  heroVideo.addEventListener("playing", function () {
+    this.classList.add("fade-in");
+    this.classList.remove("fade-out");
+  });
 
-	//pricing-video
-	const video = document.querySelector(".pricing-video")
-	video.addEventListener("ended", function () {
-		this.classList.remove("fade-in")
-		this.classList.add("fade-out")
-		setTimeout(() => video.play(), 1300)
-	})
+  //process-video
+  const processVideo = document.querySelector(".process-video");
+  processVideo.addEventListener("ended", function () {
+    this.classList.remove("fade-in");
+    this.classList.add("fade-out");
+    setTimeout(() => processVideo.play(), 1300);
+  });
 
-	video.addEventListener("playing", function () {
-		this.classList.add("fade-in")
-		this.classList.remove("fade-out")
-	})
-}
+  processVideo.addEventListener("playing", function () {
+    this.classList.add("fade-in");
+    this.classList.remove("fade-out");
+  });
+
+  //pricing-video
+  const pricingVideo = document.querySelector(".pricing-video");
+  pricingVideo.addEventListener("ended", function () {
+    this.classList.remove("fade-in");
+    this.classList.add("fade-out");
+    setTimeout(() => pricingVideo.play(), 1300);
+  });
+
+  pricingVideo.addEventListener("playing", function () {
+    this.classList.add("fade-in");
+    this.classList.remove("fade-out");
+  });
+};
 
 // https://flickity.metafizzy.co/
 // external js: flickity.pkgd.js
